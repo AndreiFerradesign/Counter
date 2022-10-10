b4512ee8cd0e19ae298528a030a9e877af75d63e
AndreiFerradesign <114855224+AndreiFerradesign@users.noreply.github.com>
2022-10-10 09:43:09 +0300

    реализовано решение задачи по созданию счетчика по нажатию кнопки

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonCounter: UIButton!
+    @IBOutlet weak var labelCounter: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        labelCounter.text = "Значение счётчика:  0"
+        buttonCounter.titleLabel?.textAlignment = .center
+        
     }
+    
+    private var howManyClick: Int = 0
+    private var counterText: String = "Значение счётчика:"
 
-
+    @IBAction func buttonClick(_ sender: Any) {
+        howManyClick += 1
+        labelCounter.text = "\(counterText)  \(howManyClick)"
+        
+    }
 }
 
